ec683c63107266c9c59730ed94304a799bac44c1
unknown <33537@ETECAF.LOCAL>
2019-03-15 17:41:42 -0300

    first commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public int j;
+    public double d;
+    public string s;
+    void concatenar(int i, double e, string t)
+    {
+        Debug.Log(i + j);
+        Debug.Log(d + e);
+        Debug.Log(t.ToString() + s.ToString());
+    }
+
+	void Start () {
+
+        concatenar(15,4.5,"ro");
+	}
+
+}
---
+++ Assets/Scripts/meintwo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class meintwo : MonoBehaviour {
+
+
+     public double Cstref = 12;
+     public int Pergor = 20;
+     public int Perimp = 8;
+    
+    void Start ()
+    {
+        soma(12,20,8 );
+	}
+    void soma(double Cstref, int Pergor, int Perimp)
+    {
+        double gorjeto = Cstref * ((double)Pergor / 100);
+        double imposto = Cstref * ((double)Perimp / 100);
+        var custototal = Cstref + gorjeto + imposto;
+
+        Debug.Log(Math.Round(custototal));
+    }
+    
+}
